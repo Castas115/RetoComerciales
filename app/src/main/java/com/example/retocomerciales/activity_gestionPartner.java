8f55e01e89419f04aed42d10614c081f489831b6
haxixa <56109212+haxixa@users.noreply.github.com>
2020-12-22 21:10:12 +0100

    llamada

--- app/src/main/java/com/example/retocomerciales/activity_gestionPartner.java
+++ app/src/main/java/com/example/retocomerciales/activity_gestionPartner.java
@@ -1,14 +1,44 @@
 package com.example.retocomerciales;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_gestionPartner extends AppCompatActivity {
 
-    @Override
+
+    Button alta,volver;
+    Intent intent;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_gestion_partner);
+
+        //findViewById
+        alta = findViewById(R.id.btnAlta);
+        volver = findViewById(R.id.btnVolver);
+
+
+        alta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(activity_gestionPartner.this, activity_addPartner.class);
+                startActivity(intent);
+            }
+        });
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(activity_gestionPartner.this, activity_menu.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
