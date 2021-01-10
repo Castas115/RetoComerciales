a988e916edc5bc06c784fd03cd7df8a51c34e448
Mikel <mikel2dm3@gmail.com>
2021-01-10 17:03:59 +0100

    -Metodo existencias

--- app/src/main/java/com/example/retocomerciales/Clases/Producto.java
+++ app/src/main/java/com/example/retocomerciales/Clases/Producto.java
@@ -1,6 +1,11 @@
 package com.example.retocomerciales.Clases;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Cada producto individual. Dato cargado del XML/base de datos
@@ -58,5 +63,31 @@ public class Producto implements Serializable {
     public void ajustarExistenciasCompra(){
         this.existenciasCompra = this.existencias;
     }
+
+    //convertire el contenido de Partners en un elemento XML
+    public Element toElement(Document document) {
+        ArrayList<Element> elements = new ArrayList<>(Arrays.asList(
+                document.createElement("producto"),
+                document.createElement("nombre"),
+                document.createElement("imagen"),
+                document.createElement("descripcion"),
+                document.createElement("precio"),
+                document.createElement("existencias")
+        ));
+
+        elements.get(0).setAttribute("cod", cod);
+        elements.get(1).setTextContent(nombre);
+        elements.get(2).setTextContent(imagen);
+        elements.get(3).setTextContent(descripcion);
+        elements.get(4).setTextContent(String.valueOf(pr_unidad));
+        elements.get(5).setTextContent(String.valueOf(existencias));
+
+        //el elemento 0 es el elemento padre
+        for(int i = 1; i < elements.size(); i++) {
+            elements.get(0).appendChild(elements.get(i));
+        }
+        return elements.get(0);
+    }
+
 
 }
